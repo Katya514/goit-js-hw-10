f355b1c23b511d51d2fa3eb0d1ec048c30c0bcd9
Katya514 <rutkovskaaa@gmail.com>
2023-03-13 12:56:44 -0600

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,23 +1,6 @@
-// import Notiflix from 'notiflix';
-
-// export function fetchCountries(countryName) {
-//   return fetch(
-//     'https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages'
-//   ).then(response => {
-//     if (!response.ok) {
-//       return Notiflix.Notify.failure(
-//         'Oops, there is no country with that name'
-//       );
-//     }
-//     return response.json();
-//   });
-// }
-
-const URL = 'https://restcountries.com/v3.1/name';
-
 export function fetchCountries(name) {
   return fetch(
-    `${URL}/${name}?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
       throw new Error('Data fail!');
